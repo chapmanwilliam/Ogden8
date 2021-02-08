377639121f911a0f9051e3859c5e7b9b02dc2f09
chapmanwilliam <wchapman10@gmail.com>
2021-02-08 15:49:56 +0000

    rereferencing dependencies - with json conversion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from localpackage.game import game
 import json
 
+
+
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
@@ -10,6 +12,18 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    row = {'fromAge': 35, 'toAge': 60, 'freq': 'Y', 'name': 'Uninjured', 'cont': 1, 'options': 'AMI'}
+    rows = [row for a in range(1, 20)]
+    eg = {"rows": rows, "discountRate": -0.005, "Ogden": 7, "claimants": [
+        {"age": 55, "aai": 24.999315537303218, "sex": "Female",
+         "dataSet": {"year": 2008, "region": "UK", "yrAttainedIn": 2011}, "deltaLEB": -15, "deltaLEA": -15}],
+          "dependents": [{"age": 40, "sex": "Male", "dataSet": {"year": 2008, "region": "UK", "yrAttainedIn": 2011}}]}
+
+    gamejs = json.dumps(eg)
+    attributes = json.loads(gamejs)
+    g = game(attributes=attributes)
+    rows = attributes['rows']
+
     request_json = request.get_json()
 #    x=json.loads(request_json)
 #    g = game(attributes=x)
