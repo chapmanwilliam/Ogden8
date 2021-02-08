a6e6f6d00e89d615deb56237200a75741ec5db1f
chapmanwilliam <wchapman10@gmail.com>
2021-02-08 17:43:25 +0000

    rereferencing dependencies - working version!

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ def hello_world(request):
 
     request_json = request.get_json()
 
-    return request_json
-
     attributes=json.loads(request_json)
     g = game(attributes=attributes)
     rows = attributes['rows']
