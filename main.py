bd40d0a365bab7705559d5a5782dbae8037d6b51
chapmanwilliam <wchapman10@gmail.com>
2021-02-09 16:40:52 +0000

    fast api

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from localpackage.game import game
+from fastapi import FastAPI
 import json
 
+app=FastAPI()
 
-
+@app.post("/Multiplier/")
 def Multiplier(request):
     """Responds to any HTTP request.
     Args:
@@ -26,26 +28,3 @@ def Multiplier(request):
 
     return json.dumps(a)
 
-
-def JointMultiplier(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-
-    request_json = request.get_json() #gets json string from request
-
-    if isinstance(request_json,dict):
-        attributes=request_json #it is already a dictionary
-    else:
-        attributes=json.loads(request_json) #takes a json string and loads it into python dictionary
-
-    g = game(attributes=attributes)
-    rows = attributes['rows']
-    a = [g.dependents[row['name']].MJ(row['fromAge'], row['toAge'],freq=row['freq'], cont=row['cont'], options=row['options']) for row in rows]
-
-    return json.dumps(a)
