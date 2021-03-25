fd4b8070892fb32031afc4d975bb633c9609eea8
chapmanwilliam <wchapman10@gmail.com>
2021-03-25 22:24:49 +0000

    CORS enabled function

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ def Multiplier(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
 
     request_json = request.get_json() #gets json string from request
 
@@ -25,5 +42,5 @@ def Multiplier(request):
 
     #returns an array of tuples (past,interest,future,total), one for each row
 
-    return json.dumps(game(attributes=attributes).processRows())
+    return (json.dumps(game(attributes=attributes).processRows()), 200, headers)
 
